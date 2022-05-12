9537dfca7ad88e6a88a94ad2a5260309491c3957
Ferdinand Malcher <ferdinand@malcher.media>
2022-05-12 15:13:00 +0200

    Dashboard: Service verwenden

--- book-rating/src/app/books/dashboard/dashboard.component.ts
+++ book-rating/src/app/books/dashboard/dashboard.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Book } from '../shared/book';
+import { BookRatingService } from '../shared/book-rating.service';
 
 @Component({
   selector: 'br-dashboard',
@@ -10,7 +11,7 @@ export class DashboardComponent implements OnInit {
 
   books: Book[] = [];
 
-  constructor() {
+  constructor(private rs: BookRatingService) {
     this.books = [
       {
         isbn: '123',
@@ -33,11 +34,32 @@ export class DashboardComponent implements OnInit {
   ngOnInit(): void {}
 
   doRateUp(book: Book) {
-    console.log('UP', book);
+    const ratedBook = this.rs.rateUp(book);
+    this.updateList(ratedBook);
   }
 
   doRateDown(book: Book) {
-    console.log('DOWN', book);
+    const ratedBook = this.rs.rateDown(book);
+    this.updateList(ratedBook);
+  }
+
+  private updateList(ratedBook: Book) {
+    this.books = this.books.map(book => {
+      if (book.isbn === ratedBook.isbn) {
+        return ratedBook;
+      } else {
+        return book;
+      }
+    });
+
+    // this.books = this.books.map(book => book.isbn === ratedBook.isbn ? ratedBook : book);
+
+    /*const copiedList = [...this.books];
+    copiedList[i] = ratedBook;
+    this.books = copiedList;*/
+
+    // [1,2,3,4,5,6,7].filter(e => e > 3); // [4,5,6,7]
+    // [1,2,3,4,5].map(e => e * 10) // [10, 20, 30, 40, 50]
   }
 }
 
