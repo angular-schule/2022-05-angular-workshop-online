72914a8f20534bb0f11640590a623877148f72f0
Ferdinand Malcher <ferdinand@malcher.media>
2022-05-17 13:30:13 +0200

    RxJS: Creating

--- rxjs-playground/src/app/exercises/creating/creating.component.ts
+++ rxjs-playground/src/app/exercises/creating/creating.component.ts
@@ -22,7 +22,70 @@ export class CreatingComponent {
 
     /******************************/
 
-    
+
+
+    // of('Angular', 'Vue', 'React')
+    // from(['A', 'B', 'C'])
+    // interval(1000) // ---0---1---2---3---4--- ...
+    // timer(3000) // ---------0|
+    // timer(3000, 1000) // ---------0---1---2---3---4--- ...
+
+    timer(0, 1000).pipe(
+      map(e => e * 3),
+      filter(e => e % 2 === 0)
+    ).subscribe({
+      next: e => this.log(e),
+      complete: () => this.log('COMPLETE')
+    });
+
+
+
+    /******************************/
+
+
+    function producer(sub: any) {
+      const result = Math.random();
+      sub.next(result);
+      sub.next(6);
+
+      const iii = setInterval(() => {
+        // cberr('FEHLER!');
+        console.log('TIMER');
+        sub.next(Math.random());
+      }, 1000);
+
+      // Teardown Logic
+      return () => {
+        clearInterval(iii);
+      };
+    }
+
+    const obs = {
+      next: (e: any) => console.log(e),
+      // error: (err: any) => console.error(err),
+      complete: () => console.log('FERTIG!')
+    };
+
+    // producer(obs);
+    // Finnische Notation $
+    // const myObs$ = new Observable(producer);
+    // const sub = myObs$.subscribe(obs);
+
+    setTimeout(() => {
+      // sub.unsubscribe();
+    }, 5000)
+
+
+    /*class MyObservable {
+      constructor(private producer: any) {}
+
+      subscribe(observer: any) {
+        const subscriber = this.sanitizeObserver(observer);
+        this.producer(subscriber);
+      }
+    }*/
+
+
     /******************************/
   }
 
