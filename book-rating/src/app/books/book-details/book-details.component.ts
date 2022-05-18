40e1013606257f689fd23520baf4f99c56f95183
Ferdinand Malcher <ferdinand@malcher.media>
2022-05-18 10:33:58 +0200

    Detailseite: Refactoring RxJS

--- book-rating/src/app/books/book-details/book-details.component.ts
+++ book-rating/src/app/books/book-details/book-details.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { concatMap, map, switchMap } from 'rxjs';
 import { Book } from '../shared/book';
 import { BookStoreService } from '../shared/book-store.service';
 
@@ -18,13 +19,15 @@ export class BookDetailsComponent implements OnInit {
     // console.log(isbn);
 
     // Asychroner Weg // PUSH
-    // TODO: Verschachtelte Subscriptions vermeiden!
-    this.route.paramMap.subscribe(params => {
-      const isbn = params.get('isbn')!; // Non-Null Assertion
-      this.bs.getSingle(isbn).subscribe(book => {
-        this.book = book;
-      });
+
+    this.route.paramMap.pipe(
+      map(params => params.get('isbn')!),
+      switchMap(isbn => this.bs.getSingle(isbn))
+    ).subscribe(book => {
+      this.book = book;
     });
+
+
   }
 
   ngOnInit(): void {
